bbc35892a5b42e08130e55890ae696be8e232349
이혜린 <46877318+hyerinlee@users.noreply.github.com>
2020-12-04 17:49:22 +0900

    Update 모의고사.cpp

--- level1/모의고사.cpp
+++ level1/모의고사.cpp
@@ -1,24 +1,20 @@
 #include <string>
 #include <vector>
-#define max(a,b) ((a)>(b)? (a):(b))
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
-    vector<int> a={1,2,3,4,5};
-    vector<int> b={2,1,2,3,2,4,2,5};
-    vector<int> c={3,3,1,1,2,2,4,4,5,5};
+    vector<vector<int>> student = {{1,2,3,4,5}, {2,1,2,3,2,4,2,5}, {3,3,1,1,2,2,4,4,5,5}};
+    vector<int> cnt={0,0,0};
     vector<int> answer;
-    int m=0,as=0,bs=0,cs=0;
-    for(int i=0; i<answers.size(); i++){
-	    if(answers[i]==a[i%5]) as++;
-	    if(answers[i]==b[i%8]) bs++;
-	    if(answers[i]==c[i%10]) cs++;
+    int max=0;
+    for(int j=0; j<student.size(); j++){
+        for(int i=0; i<answers.size(); i++){
+            if(answers[i]==student[j][i%student[j].size()]) cnt[j]++;
+        }
     }
-    m=max(as,max(bs,cs));
-    if(m==as) answer.push_back(1);
-    if(m==bs) answer.push_back(2);
-    if(m==cs) answer.push_back(3);
-
+    max=*max_element(cnt.begin(), cnt.end());
+    for(int i=0; i<cnt.size(); i++) if(max==cnt[i]) answer.push_back(i+1);
     return answer;
 }
