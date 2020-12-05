d25d40fb3d37526b631506dd981d566afd4b2cb0
이혜린 <46877318+hyerinlee@users.noreply.github.com>
2020-12-05 23:52:12 +0900

    Create 수박수박수박수박수박수.cpp

---
+++ level1/수박수박수박수박수박수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for(int i=1;i<=n;i++){
+        (i%2!=0)? answer.append("수"):answer.append("박");
+    }
+    return answer;
+}
