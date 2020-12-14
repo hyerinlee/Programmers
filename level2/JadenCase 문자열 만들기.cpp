8af44f3d66a4bf252541e6d3adc6b5af7ed975a9
이혜린 <46877318+hyerinlee@users.noreply.github.com>
2020-12-14 23:46:50 +0900

    Create JadenCase 문자열 만들기.cpp

---
+++ level2/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+string solution(string s) {
+    s[0]=toupper(s[0]);
+    for(int i=1; i<s.size(); i++){
+        if(s[i-1]==' ') s[i]=toupper(s[i]);
+        else s[i]=tolower(s[i]);
+    }
+    return s;
+}
