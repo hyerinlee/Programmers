0b1949e4df8188866c96b2ada8896cadf0705b4b
이혜린 <46877318+hyerinlee@users.noreply.github.com>
2020-12-16 23:44:25 +0900

    Update x만큼 간격이 있는 n개의 숫자.cpp

--- level1/x만큼 간격이 있는 n개의 숫자.cpp
+++ level1/x만큼 간격이 있는 n개의 숫자.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 vector<long long> solution(int x, int n) {
     vector<long long> answer;
-    answer.push_back(x);
-    for(int i=1;i<n;i++){
-        answer.insert(answer.begin()+i, answer[i-1]+x);
-    }
+    for(int i=1; i<=n; i++) answer.push_back(x*i);
     return answer;
 }
