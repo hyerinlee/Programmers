1153ebe5736bdac45d1ab619e9451f136dab2a7e
이혜린 <46877318+hyerinlee@users.noreply.github.com>
2020-12-16 23:54:08 +0900

    Update 핸드폰 번호 가리기.cpp

--- level1/핸드폰 번호 가리기.cpp
+++ level1/핸드폰 번호 가리기.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 string solution(string phone_number) {
-    string answer;
-    for(int i=0; i<phone_number.size()-4; i++){
-        answer.append("*");
-    }
-    answer.append(phone_number.substr(phone_number.size()-4,4));
-    return answer;
+    for(int i=0; i<phone_number.size()-4; i++) phone_number[i]='*';
+    return phone_number;
 }
