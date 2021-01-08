49bfad0c3afa26d8a50ee100fd73b7b00a273d75
이혜린 <46877318+hyerinlee@users.noreply.github.com>
2021-01-08 22:54:21 +0900

    Update 카펫.cpp

--- level2/카펫.cpp
+++ level2/카펫.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
-    int sum = brown/2 + 2;
-    for(int i=3; i<sum; i++){
-        if(((i-2)*(sum-i-2))==yellow){
-            (i>sum-i)? answer.push_back(i) : answer.push_back(sum-i);
-            answer.push_back(sum-answer[0]);
-            return answer;
-        }
+    int sum = brown/2 + 2;  //x+y
+    int x=sum, y=0, area=brown+yellow;
+    while(x*y!=area){
+        x--;
+        y++;
     }
+    return {x,y};
 }
